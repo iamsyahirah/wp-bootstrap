f837cceffad2fe73003cf4f5a7396b0ef6f60cde
syahirah <syahirahmohammad89@gmail.com>
2017-02-11 20:10:30 +0800

    add header and footer php in separate file

--- index.php
+++ index.php
@@ -1,47 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header(); ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Blog Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-
-</head>
-
-<body>
-
-    <div class="blog-masthead">
-        <div class="container">
-            <nav class="blog-nav">
-                <a class="blog-nav-item active" href="#">Home</a>
-                <a class="blog-nav-item" href="#">New features</a>
-                <a class="blog-nav-item" href="#">Press</a>
-                <a class="blog-nav-item" href="#">New hires</a>
-                <a class="blog-nav-item" href="#">About</a>
-            </nav>
-        </div>
-    </div>
-
-    <div class="container">
-
-        <div class="blog-header">
-            <h1 class="blog-title">The Bootstrap Blog</h1>
-            <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
-        </div>
-
-        <div class="row">
+ <div class="row">
 
             <div class="col-sm-8 blog-main">
 
@@ -160,15 +119,4 @@
     </div>
     <!-- /.container -->
 
-    <footer class="blog-footer">
-        <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-        <p>
-            <a href="#">Back to top</a>
-        </p>
-    </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-</body>
-
-</html>
+    <?php get_footer(); ?>
