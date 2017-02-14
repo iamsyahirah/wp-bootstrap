4a35772795dd82a0ced96590a99606710029b5f9
syahirah <syahirahmohammad89@gmail.com>
2017-02-14 21:44:24 +0800

    add excerpt length and theme post thumbnails

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ require_once('wp_bootstrap_navwalker.php');
 
 // Theme Support
 function wpb_theme_setup(){
+ add_theme_support( 'post-thumbnails' ); 
+
  // Nav Menus
  register_nav_menus(array(
   'primary' => __('Primary Menu')
@@ -11,4 +13,28 @@ function wpb_theme_setup(){
 }
 
 add_action('after_setup_theme','wpb_theme_setup');
+
+// Excerpt Length Control
+function set_excerpt_length(){
+ return 40;
+}
+
+add_filter('excerpt_length','set_excerpt_length');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
